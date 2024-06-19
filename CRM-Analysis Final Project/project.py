a3d75bf668ac51940460b276e125819e25f1b6c4
Anil Yasar <anlysr2002@gmail.com>
2024-06-19 08:51:17 +0300

    scraping category pages to take book detail page links

--- CRM-Analysis Final Project/project.py
+++ CRM-Analysis Final Project/project.py
@@ -13,14 +13,37 @@ driver = webdriver.Chrome(options)
 driver.get("https://books.toscrape.com/index.html")
 time.sleep(2)
 
-category_links = driver.find_elements(By.XPATH, "//a[contains(@href, 'travel') or (contains(@href, 'nonfiction'))]")
+category_links_raw = driver.find_elements(By.XPATH, "//a[contains(@href, 'travel') or (contains(@href, 'nonfiction'))]")
+category_links = list()
 travel_link = None
 nonfiction_link = None
-for l in category_links:
+for l in category_links_raw:
     innerText = l.get_attribute("innerText").strip()
     if innerText == 'Travel':
         travel_link = l.get_attribute("href")
     elif innerText == 'Nonfiction':
         nonfiction_link = l.get_attribute("href")
+
+category_links.append(travel_link)
+category_links.append(nonfiction_link)
+
+travel_books_details = []
+nonfiction_books_details = []
+
+for l in category_links:
+    driver.get(l)
+    time.sleep(2)
+    book_details = driver.find_elements(By.XPATH, "//div[contains(@class, 'image_container')]/a")
+    for book in book_details:
+        if 'travel' in l:
+            travel_books_details.append(book.get_attribute("href"))
+        elif 'nonfiction' in l:
+            nonfiction_books_details.append(book.get_attribute("href"))
+
+
+
+
+
+
 
 
